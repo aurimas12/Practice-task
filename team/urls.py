989b522456dfdf9421d326e77457ea3a66359407
aurimas <aurimas.butk@gmail.com>
2021-04-19 19:45:09 +0300

    update code

--- team/urls.py
+++ team/urls.py
@@ -1,11 +1,11 @@
 from django.urls import include,path
-from django.conf.urls import url 
+
 from team.views import *
 
 urlpatterns = [
-    path("team/participation_all", get_participants_all),
-    path("team/participation/<int:pk>/", participant_by_id),
+    path('team/participant/all', get_participants_all),
+    path("team/participant/<int:pk>/", participant_by_id),
     path("team/participant/create", CreateParticipation.as_view()),
-    path("participant/<int:pk>/update", ParticipantUpdate.as_view()),
-    path("team/<int:pk>/delete/", participant_delete),
+    path("team/participant/<int:pk>/update", ParticipantUpdate.as_view()),
+    path("team/participant/<int:pk>/delete/", participant_delete),
 ]
