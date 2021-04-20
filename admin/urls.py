831be9b4f8a7606dad9d9bb688cdf48617c914ae
aurimas <aurimas.butk@gmail.com>
2021-04-20 10:15:29 +0300

    booking model update- create urls.py

--- admin/urls.py
+++ admin/urls.py
@@ -6,12 +6,12 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="Booking API",
       default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
+      # description="Test description",
+      # terms_of_service="https://www.google.com/policies/terms/",
+      # contact=openapi.Contact(email="contact@snippets.local"),
+      # license=openapi.License(name="BSD License"),
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
