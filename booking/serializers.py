9458c7a53c104672c8a49507c254fc849fffe349
aurimas <aurimas.butk@gmail.com>
2021-04-21 23:13:47 +0300

    PT-29: create validation date_from to endpoint booking/create

--- booking/serializers.py
+++ booking/serializers.py
@@ -1,18 +1,26 @@
 from rest_framework import serializers
-from .models import BookableType,Bookable, Booking
+from .models import BookableType, Bookable, Booking
 
 
 class BookableTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = BookableType
-        fields = '__all__'
+        fields = "__all__"
+
 
 class BookableSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bookable
-        fields = '__all__'
+        fields = "__all__"
+
 
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
-        fields = '__all__'
+        fields = "__all__"
+
+    def validate_date_from(self, value):
+        date_from = Booking.objects.filter(date_from=value)
+        if date_from.exists():
+            raise serializers.ValidationError("This date from is exist")
+        return value
--- booking/views.py
+++ booking/views.py
@@ -1,15 +1,21 @@
 from rest_framework import viewsets
-from .models import *
-from .serializers import *
+from .models import BookableType, Bookable, Booking
+from .serializers import BookableTypeSerializer, BookableSerializer, BookingSerializer
+
 
 class BookableTypeViewSet(viewsets.ModelViewSet):
-    queryset=BookableType.objects.all()
-    serializer_class= BookableTypeSerializer
+    queryset = BookableType.objects.all()
+    serializer_class = BookableTypeSerializer
+
 
 class BookableViewSet(viewsets.ModelViewSet):
-    queryset=Bookable.objects.all()
-    serializer_class= BookableSerializer
+    queryset = Bookable.objects.all()
+    serializer_class = BookableSerializer
+
 
 class BookingViewSet(viewsets.ModelViewSet):
-    queryset=Booking.objects.all()
-    serializer_class= BookingSerializer    
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+
+some = {"a": "23", "b": "6767", "c": "rer"}
