28c1901bf2bbbbc335e3320aea53d8a72ba757c1
aurimas <aurimas.butk@gmail.com>
2021-04-23 01:34:38 +0300

    PT-37: create test validate viewset create method

--- booking/tests.py
+++ booking/tests.py
@@ -3,7 +3,6 @@ from .models import Booking, Bookable, BookableType, Team, Participation
 from rest_framework import status
 from django.test.client import Client
 from django.urls import reverse
-from booking.serializers import BookableTypeSerializer
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -20,7 +19,7 @@ class ShopTestCase(TestCase):
         self.bookable = Bookable.objects.create(
             name="string", bookable_type_id=self.bookabletype, team_id=self.team
         )
-        self.data = Booking.objects.create(
+        self.booking = Booking.objects.create(
             date_from="2021-04-22T12:46:46.087Z",
             date_to="2021-04-22T12:46:46.087Z",
             bookable_id=self.bookable,
@@ -28,4 +27,28 @@ class ShopTestCase(TestCase):
         )
 
     def test_data(self):
-        self.assertEqual(2, 2)
+        valid_data = {
+            "date_from": "2021-04-22T22:14:33.575Z",
+            "date_to": "2021-04-22T22:14:33.575Z",
+            "bookable_id": 1,
+            "participant_id": 1,
+        }
+        invalid_data = {
+            "date_from": "2021-04-22T12:46:46.087Z",
+            "date_to": "2021-04-22T22:14:33.575Z",
+            "bookable_id": 1,
+            "participant_id": 1,
+        }
+        client = Client()
+        response1 = client.post(
+            reverse("booking_create"),
+            data=valid_data,
+            content_type="application/json",
+        )
+        response2 = client.post(
+            reverse("booking_create"),
+            data=invalid_data,
+            content_type="application/json",
+        )
+        self.assertEqual(response1.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response2.status_code, status.HTTP_400_BAD_REQUEST)
