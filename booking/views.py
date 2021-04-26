ea5e1072ce484fa0dd41fb5b2dcef6edde9e29c6
aurimas <aurimas.butk@gmail.com>
2021-04-26 16:08:29 +0300

    PT-46: create services request_save_data()

--- booking/views.py
+++ booking/views.py
@@ -14,11 +14,12 @@ from .serializers import (
 )
 from src.services.BookingService import check_date_from
 from src.services.BookableTypeLimitService import (
-    # get_meeting_room_count,
-    get_bookable_type_count,
+    get_workspace_count,
+    get_meeting_room_count,
     get_parking_spot_count,
     check_request_bookable_type,
     get_bookable_types_limits,
+    request_save_data,
 )
 
 
@@ -44,34 +45,25 @@ class BookableViewSet(viewsets.ModelViewSet):
 
     def create(self, request):
         serializer = BookableSerializer(data=request.data)
-
         request_bookable_type = check_request_bookable_type(request)
-        print(request_bookable_type)
-
-        parking_spot_limit = get_parking_spot_count()
         workspace, meeting_room, car_spot = get_bookable_types_limits()
 
-        if (
-            request_bookable_type == BookableType.TYPE_WORKSPACE
-        ):  # create workspace request data save
-            pass
-        elif (
-            request_bookable_type == BookableType.TYPE_MEETING_ROOM
-        ):  # create workspace request data save
-            pass
-        elif request_bookable_type == BookableType.TYPE_PARKING_SPOT:
-            if car_spot < parking_spot_limit:
-                if serializer.is_valid():
-                    serializer.save()
-                    return Response(
-                        {"msg": "Data Created"}, status=status.HTTP_201_CREATED
-                    )
-            else:
-                print("no free place")  # create response msg
-        else:
-            print("Can't booking car place! All cars spot used")  # create response msg
+        if request_bookable_type == BookableType.TYPE_WORKSPACE:
+            workspace_limit = get_workspace_count()
+            if workspace < workspace_limit:
+                request_save_data(serializer)
 
-        return Response("nieko nevyksta")
+        elif request_bookable_type == BookableType.TYPE_MEETING_ROOM:
+            meeting_room_limit = get_meeting_room_count()
+            if meeting_room < meeting_room_limit:
+                request_save_data(serializer)
+
+        elif request_bookable_type == BookableType.TYPE_PARKING_SPOT:
+            parking_spot_limit = get_parking_spot_count()
+            if car_spot < parking_spot_limit:
+                request_save_data(serializer)
+
+        return Response("Can't booking this place")
 
 
 class BookingViewSet(viewsets.ModelViewSet):
--- src/services/BookableTypeLimitService.py
+++ src/services/BookableTypeLimitService.py
@@ -1,41 +1,34 @@
 from src.exceptions.bookingException import BadRequestException
 from booking.models import BookableType, Bookable, BookableTypeLimit
+from rest_framework.response import Response
+from rest_framework import viewsets, status
+
+
+def get_workspace_count():
+    value = BookableTypeLimit.objects.all()
+    return value[0].parking_spot_limit
+
+
+def get_meeting_room_count():
+    value = BookableTypeLimit.objects.all()
+    return value[0].parking_spot_limit
 
 
 def get_parking_spot_count():
     value = BookableTypeLimit.objects.all()
-
     return value[0].parking_spot_limit
-
-
-def get_bookable_type_count(bookable_type_id):
-    bookables = Bookable.objects.all()
-    count = 0
-    for i in bookables:
-        # print("bookable id:", i.id)
-        print(1)
-        value = BookableType.objects.get(id=i.id)
-        print(value)
-        # print("bookable type", value.bookable_type, "-", bookable_type_id)
-        print("****")
-        if value.bookable_type == bookable_type_id:
-            print(value)
-
-    print(len(bookables))
-    return count
 
 
 def get_bookable_types_limits():
     workspace = 0
     meeting_room = 0
     car_spot = 0
-    # get all bookables
-    x = Bookable.objects.all()
     ids = []
+    bookables = Bookable.objects.all()
 
-    for i in x:
-        # print(i, i.id, i.bookable_type_id)
-        ids.append((i.bookable_type_id))
+    for i in bookables:
+        ids.append(i.bookable_type_id)
+
     for i in ids:
         if i.bookable_type == BookableType.TYPE_WORKSPACE:
             workspace += 1
@@ -43,15 +36,16 @@ def get_bookable_types_limits():
             meeting_room += 1
         elif i.bookable_type == BookableType.TYPE_PARKING_SPOT:
             car_spot += 1
-        print(i.bookable_type)
     return workspace, meeting_room, car_spot
 
 
 def check_request_bookable_type(request):
     request_bookable_type_id = request.data["bookable_type_id"]
-    print("*****")
+    bookable = BookableType.objects.get(id=request_bookable_type_id)
+    return bookable.bookable_type
 
-    print(request_bookable_type_id)
-    a = BookableType.objects.get(id=request_bookable_type_id)
-    print(a.bookable_type)
-    return a.bookable_type
+
+def request_save_data(serializer):
+    if serializer.is_valid():
+        serializer.save()
+        return Response({"msg": "Data Created"}, status=status.HTTP_201_CREATED)
