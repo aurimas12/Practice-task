1e24ed3fc92de56130473bcdd5b23f5296028e24
aurimas <aurimas.butk@gmail.com>
2021-05-06 21:41:41 +0300

    create django service in kong

--- script.sh
+++ script.sh
@@ -11,12 +11,12 @@ curl -i -X POST http://localhost:8001/services/ \
 --data url=http://mockbin.org/request
 
 # Create route
-curl -i -X POST http://localhost:8001/team-a/services/mockbin/routes \
+curl -i -X POST http://localhost:8001/services/mockbin/routes \
 --data "name=mockbin" \
 --data "paths[]=/mockbin"
 
 # Enable the OAuth 2.0 plugin for that service
-curl -i -X POST localhost:8001/team-a/services/mockbin/plugins \
+curl -i -X POST localhost:8001/services/mockbin/plugins \
 --data "name=oauth2" \
 --data "config.scopes=email" \
 --data "config.enable_client_credentials=true"
@@ -26,11 +26,11 @@ curl -i -X POST localhost:8001/team-a/consumers \
 --data "username=user1"
 
 # Create a key for that consumer
-curl -i -X POST localhost:8001/team-a/consumers/user1/key-auth \
+curl -i -X POST localhost:8001/consumers/user1/key-auth \
 --data "key=123"
 
 # You can then provision new OAuth 2.0 credentials (also called “OAuth applications”) by making the following HTTP request
-curl -i -X POST http://localhost:8001/team-a/consumers/user1/oauth2 \
+curl -i -X POST http://localhost:8001/consumers/user1/oauth2 \
 --data "name=demo" \
 --data "client_id=456" \
 --data "client_secret=789" \
--- test.sh
+++ test.sh
@@ -3,5 +3,8 @@ echo "Hello World"
 
 # Create service
 curl -i -X POST http://localhost:8001/services/ \
---data name=mockbin \
---data url=http://mockbin.org/request
+--data name=api \
+--data host=django \
+--data port=1337 \
+--data path=/
+
